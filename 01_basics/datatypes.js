dd8e0c91a8cee897a98c56d1597f81776a34315d
laxrana113 <36737980+laxrana113@users.noreply.github.com>
2024-07-24 00:00:00 +0000

    datatype basics

---
+++ 01_basics/datatypes.js
@@ -0,0 +1,12 @@
+"use strict"; // means treat all JS code as newer version
+
+//alert("Hello"); // we are using nodejs not browser
+let name="Laxmi Rana"; //string
+let age="11"; //integer, number
+let isLoggedIn=false; //boolean
+
+//bigint: for a big value
+//symbol: to find uniqueness
+//object: 
+console.log(typeof null); // object
+console.log(typeof undefined); //undefined
