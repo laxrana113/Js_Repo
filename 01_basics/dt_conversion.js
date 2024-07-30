7f4ac1eac3c9bba98f8800fa59cfbede832a6ac4
laxrana113 <36737980+laxrana113@users.noreply.github.com>
2024-07-30 19:52:57 +0000

    datatype conversion

---
+++ 01_basics/dt_conversion.js
@@ -0,0 +1,14 @@
+let age="a3";
+
+// below both are correct
+console.log(typeof age); //
+console.log(typeof(age));//
+
+let ageInString= String(age);
+console.log(typeof(ageInString));//string
+
+let ageInNum= Number(age);
+console.log(typeof(ageInNum));//
+console.log(ageInNum);//
+
+
